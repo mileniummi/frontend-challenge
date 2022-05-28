58c48a16f340d08960319643bfd94592de302445
Mihail <kokosh2000@mail.ru>
2022-05-29 00:11:27 +0300

    Deploy React app to GitHub Pages

---
+++ precache-manifest.f0d15e8208bd23e7dd9ef7e593b07d72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5004784aeaf42424840c",
+    "url": "/frontend-challenge/static/css/main.fee36420.chunk.css"
+  },
+  {
+    "revision": "5004784aeaf42424840c",
+    "url": "/frontend-challenge/static/js/main.5004784a.chunk.js"
+  },
+  {
+    "revision": "c316d995a74ba89ea236",
+    "url": "/frontend-challenge/static/js/1.c316d995.chunk.js"
+  },
+  {
+    "revision": "1a1a42a816e75f9bfd1c",
+    "url": "/frontend-challenge/static/js/runtime~main.1a1a42a8.js"
+  },
+  {
+    "revision": "c0fc527eef05e1b471e32401a9312123",
+    "url": "/frontend-challenge/index.html"
+  }
+];
