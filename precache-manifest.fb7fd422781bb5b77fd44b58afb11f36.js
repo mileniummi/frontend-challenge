a4c9a309777f02b1be6d09a7412a1a04180aaf29
Mihail <kokosh2000@mail.ru>
2022-05-29 22:19:58 +0300

    Updates

---
+++ precache-manifest.fb7fd422781bb5b77fd44b58afb11f36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7f33429a96efdd91e2a",
+    "url": "/frontend-challenge/static/css/main.5e81d61e.chunk.css"
+  },
+  {
+    "revision": "b7f33429a96efdd91e2a",
+    "url": "/frontend-challenge/static/js/main.b7f33429.chunk.js"
+  },
+  {
+    "revision": "180079a8ffdd3f3dd397",
+    "url": "/frontend-challenge/static/js/1.180079a8.chunk.js"
+  },
+  {
+    "revision": "1a1a42a816e75f9bfd1c",
+    "url": "/frontend-challenge/static/js/runtime~main.1a1a42a8.js"
+  },
+  {
+    "revision": "5e0f3f3bd820f8ec530adef47516ed16",
+    "url": "/frontend-challenge/static/media/error-cat.5e0f3f3b.jpg"
+  },
+  {
+    "revision": "f1a4675c59d1c6244f2026a213206658",
+    "url": "/frontend-challenge/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/frontend-challenge/precache-manifest.3c3bd7574ea3ab5e3edd6dda79c7c2aa.js"
+  "/frontend-challenge/precache-manifest.fb7fd422781bb5b77fd44b58afb11f36.js"
 );
 
 workbox.clientsClaim();
